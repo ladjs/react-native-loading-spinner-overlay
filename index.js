095e5ed45345404507b102d9a7f39c62fe8ffcba
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2021-08-28 17:33:06 +0700

    Upgrade React Native version and change to hook

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 // <https://github.com/brentvatne/react-native-overlay>
 //
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, View, Text, Modal, ActivityIndicator } from 'react-native';
 
@@ -62,110 +62,118 @@ const styles = StyleSheet.create({
 const ANIMATION = ['none', 'slide', 'fade'];
 const SIZES = ['small', 'normal', 'large'];
 
-export default class Spinner extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: this.props.visible,
-      textContent: this.props.textContent
-    };
-  }
+const Spinner = props => {
+  const [visible, setVisible] = useState(props?.visible);
+  const [textContent, setTextContent] = useState(props?.textContent);
 
-  static propTypes = {
-    cancelable: PropTypes.bool,
-    color: PropTypes.string,
-    animation: PropTypes.oneOf(ANIMATION),
-    overlayColor: PropTypes.string,
-    size: PropTypes.oneOf(SIZES),
-    textContent: PropTypes.string,
-    textStyle: PropTypes.object,
-    visible: PropTypes.bool,
-    indicatorStyle: PropTypes.object,
-    customIndicator: PropTypes.element,
-    children: PropTypes.element,
-    spinnerKey: PropTypes.string
-  };
+  useEffect(
+    () => {
+      setVisible(props?.visible);
+    },
+    [props?.visible]
+  );
+  useEffect(
+    () => {
+      setTextContent(props?.textContent);
+    },
+    [props?.textContent]
+  );
 
-  static defaultProps = {
-    visible: false,
-    cancelable: false,
-    textContent: '',
-    animation: 'none',
-    color: 'white',
-    size: 'large', // 'normal',
-    overlayColor: 'rgba(0, 0, 0, 0.25)'
-  };
+  const onClose = useCallback(() => {
+    setVisible(false);
+  }, []);
 
-  close() {
-    this.setState({ visible: false });
-  }
+  const _handleOnRequestClose = useCallback(
+    () => {
+      if (props.cancelable) {
+        onClose();
+      }
+    },
+    [props?.cancelable]
+  );
 
-  static getDerivedStateFromProps(props, state) {
-    const newState = {};
-    if (state.visible !== props.visible) newState.visible = props.visible;
-    if (state.textContent !== props.textContent)
-      newState.textContent = props.textContent;
-    return newState;
-  }
-
-  _handleOnRequestClose() {
-    if (this.props.cancelable) {
-      this.close();
-    }
-  }
-
-  _renderDefaultContent() {
-    return (
+  const _renderDefaultContent = useCallback(
+    () => (
       <View style={styles.background}>
-        {this.props.customIndicator ? (
-          this.props.customIndicator
+        {props.customIndicator ? (
+          props.customIndicator
         ) : (
           <ActivityIndicator
-            color={this.props.color}
-            size={this.props.size}
-            style={[styles.activityIndicator, { ...this.props.indicatorStyle }]}
+            color={props.color}
+            size={props.size}
+            style={[styles.activityIndicator, { ...props.indicatorStyle }]}
           />
         )}
-        <View style={[styles.textContainer, { ...this.props.indicatorStyle }]}>
-          <Text style={[styles.textContent, this.props.textStyle]}>
-            {this.state.textContent}
+        <View style={[styles.textContainer, { ...props.indicatorStyle }]}>
+          <Text style={[styles.textContent, props.textStyle]}>
+            {textContent}
           </Text>
         </View>
       </View>
-    );
-  }
+    ),
+    [
+      props.customIndicator,
+      props.color,
+      props.size,
+      props.indicatorStyle,
+      props.textStyle,
+      textContent
+    ]
+  );
 
-  _renderSpinner() {
-    const spinner = (
+  const spinner = useCallback(
+    () => (
       <View
-        style={[styles.container, { backgroundColor: this.props.overlayColor }]}
-        key={
-          this.props.spinnerKey
-            ? this.props.spinnerKey
-            : `spinner_${Date.now()}`
-        }
+        style={[styles.container, { backgroundColor: props?.overlayColor }]}
+        key={props.spinnerKey ? props.spinnerKey : `spinner_${Date.now()}`}
       >
-        {this.props.children
-          ? this.props.children
-          : this._renderDefaultContent()}
+        {props.children ? props.children : _renderDefaultContent()}
       </View>
-    );
+    ),
+    [
+      _renderDefaultContent,
+      props?.overlayColor,
+      props.children,
+      props.spinnerKey
+    ]
+  );
+  return (
+    <Modal
+      animationType={props.animation}
+      onRequestClose={_handleOnRequestClose}
+      supportedOrientations={['landscape', 'portrait']}
+      transparent
+      visible={visible}
+      statusBarTranslucent={true}
+    >
+      {spinner()}
+    </Modal>
+  );
+};
 
-    return (
-      <Modal
-        animationType={this.props.animation}
-        onRequestClose={() => this._handleOnRequestClose()}
-        supportedOrientations={['landscape', 'portrait']}
-        transparent
-        visible={this.state.visible}
-        statusBarTranslucent={true}
-      >
-        {spinner}
-      </Modal>
-    );
-  }
+Spinner.propTypes = {
+  cancelable: PropTypes.bool,
+  color: PropTypes.string,
+  animation: PropTypes.oneOf(ANIMATION),
+  overlayColor: PropTypes.string,
+  size: PropTypes.oneOf(SIZES),
+  textContent: PropTypes.string,
+  textStyle: PropTypes.object,
+  visible: PropTypes.bool,
+  indicatorStyle: PropTypes.object,
+  customIndicator: PropTypes.element,
+  children: PropTypes.element,
+  spinnerKey: PropTypes.string
+};
 
-  render() {
-    return this._renderSpinner();
-  }
-}
+Spinner.defaultProps = {
+  visible: false,
+  cancelable: false,
+  textContent: '',
+  animation: 'none',
+  color: 'white',
+  size: 'large', // 'normal',
+  overlayColor: 'rgba(0, 0, 0, 0.25)'
+};
+
+export default Spinner;
